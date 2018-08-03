8551fa524282cc7ab1b3ef0aa128cff4e190e2e9
luca_bonfiglioli <luca.bonfiglioli@gmail.com>
2018-08-03 11:33:33 +0200

    Experiments changing GA.fitness(), minor refactoring

--- Bot_Ticelli/Controller.js
+++ Bot_Ticelli/Controller.js
@@ -4,30 +4,9 @@
 
 //MAIN
 
-var pol1 = new Polygon([{x:300, y:100},{x:400, y:50},{x:500, y:100},{x:450, y:200},{x:400, y:300},{x:300, y:200}], new Color(0, 255, 0));
-var pol2 = new Polygon([{x:500, y:200},{x:600, y:150},{x:900, y:1000}], new Color(0, 0, 255));
-var pol3 = new Polygon([{x:1300, y:200},{x:1200, y:50},{x:700, y:600}], new Color(255, 255, 0));
-var pol4 = new Polygon([{x:500, y:500},{x:500, y:600},{x:600, y:600},{x:600,y:500}], new Color(255, 0, 0));
-//var comp = new PolygonComposition([pol1, pol1, pol1, pol1, pol1, pol1, pol1, pol1, pol1, pol1]);
-//var comp = new PolygonComposition([pol1, pol2, pol3, pol4]);
-var comp = PolygonComposition.random(MAX_POLYGONS);
-
-var ticks = 0;
-
-var ga = new GA(new PolygonGenerationFactory());
-ga.composition = comp;
-ga.generation = ga.factory.newGeneration(comp);
-
-var c = document.getElementById("canvas");
-var ctx = c.getContext("2d");
-var view = new View(ctx);
-
 function update(progress) {
-	ticks += progress;
-
 	ga.nextGeneration();
 	comp = ga.composition;
-
 }
 
 function draw() {
@@ -41,8 +20,28 @@ function loop(timestamp) {
 	draw();
 
 	lastRender = timestamp;
+	if (!stopflag)
+		window.requestAnimationFrame(loop);
+}
+
+function start() {
+	stopflag = false;
 	window.requestAnimationFrame(loop);
 }
 
+function stop() {
+	stopflag = true;
+}
+
+var comp = PolygonComposition.random(MAX_POLYGONS);
+var ga = new GA(new PolygonGenerationFactory());
+ga.composition = comp;
+ga.generation = ga.factory.newGeneration(comp);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+var view = new View(ctx);
+
+var stopflag = false;
 var lastRender = 0;
-window.requestAnimationFrame(loop);
+start();
--- Bot_Ticelli/GA.js
+++ Bot_Ticelli/GA.js
@@ -11,15 +11,61 @@ class GA {
 		this.generation = new Generation([]);
 		this.composition = new PolygonComposition([]);
 		this.mutationProbability = DEFAULT_MUTATION_PROBABILITY;
-		
 	}
 
+	
+// 	Color fitness
+	
+//	fitness(index) {
+//		var color = this.composition.polygons[index].color;
+//		return 255 + 255 + 255 - (color.r() + color.g() + color.b());
+//	}
+
+	
+// 	Rectangle fitness 
+	
+//	fitness(index) {
+//		var pol = this.composition.polygons[index];
+//		
+//		if (pol.vertices.length != 4) 
+//			return 0;
+//		
+//		return 1 / (Math.abs(pol.vertices[0].x - pol.vertices[1].x) + 
+//				Math.abs(pol.vertices[2].x - pol.vertices[3].x) +
+//				Math.abs(pol.vertices[0].y - pol.vertices[3].y) +
+//				Math.abs(pol.vertices[1].y - pol.vertices[2].y));
+//	}
+
+	// Must a generation ALWAYS converge?
+	
 	fitness(index) {
-		var color = this.composition.polygons[index].color;
-		return 255 + 255 + 255 - (color.r() + color.g() + color.b());
-//		return (color.r() + color.g() + color.b());
-	}
+		var pol = this.composition.polygons[index];
 
+		if (pol.vertices.length != 4)
+			return 0;
+
+		if (index % 2 == 0) {
+			return 1 / (Math.abs(pol.vertices[0].x - 100) + 
+					Math.abs(pol.vertices[1].x - 200) +
+					Math.abs(pol.vertices[2].x - 200) +
+					Math.abs(pol.vertices[3].x - 100) + 
+					Math.abs(pol.vertices[0].y - 100) + 
+					Math.abs(pol.vertices[1].y - 100) + 
+					Math.abs(pol.vertices[2].y - 200) + 
+					Math.abs(pol.vertices[3].y - 200));
+		}
+		else {
+			return 1 / (Math.abs(pol.vertices[0].x - 900) + 
+					Math.abs(pol.vertices[1].x - 1000) +
+					Math.abs(pol.vertices[2].x - 1000) +
+					Math.abs(pol.vertices[3].x - 900) + 
+					Math.abs(pol.vertices[0].y - 900) + 
+					Math.abs(pol.vertices[1].y - 900) + 
+					Math.abs(pol.vertices[2].y - 1000) + 
+					Math.abs(pol.vertices[3].y - 1000));
+		}
+	}
+	
 	mutate(ch) {
 		var r = 0;
 		for (var i = 0; i < ch.bits.length(); i++) {
@@ -54,7 +100,7 @@ class GA {
 			ch1 = this.generation.chromosomes[ch1].clone();
 			ch2 = this.generation.chromosomes[ch2].clone();
 			
-//			this.crossover(ch1,ch2);
+			this.crossover(ch1,ch2);
 			this.mutate(ch1);
 			this.mutate(ch2);
 
