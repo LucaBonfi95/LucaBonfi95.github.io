395d2f46e9dffb9cd6c4721cae2b30e068fd2317
Tzsack <Luca.Bonfiglioli@gmail.com>
2018-10-18 16:38:09 +0200

    Pixels normalization is now independent for each color channel, this results in more vivid colors. Minor refactoring.

--- Bot_Ticelli/js/genotypes/ExpressionGenotypeDecoder.js
+++ Bot_Ticelli/js/genotypes/ExpressionGenotypeDecoder.js
@@ -21,7 +21,7 @@ class RawImageExpressionGenotypeDecoder extends ExpressionGenotypeDecoder {
 	}
 	
 	decode(expressionGenotype) {
-		var x1, y1, pixel, index, temp, lambda, res1, res2, width, height, canvas, rawImage, zoom, polar, samples, pixels, grayscale;
+		var x1, y1, pixel, index, temp, lambda, res1, res2, width, height, canvas, rawImage, zoom, polar, samples, pixels, grayscale, channels;
 		canvas = new OffscreenCanvas(this.width, this.height);
 		this.ctx = canvas.getContext("2d");
 		rawImage = new RawImage(this.ctx.getImageData(0, 0, this.width, this.height));
@@ -30,76 +30,74 @@ class RawImageExpressionGenotypeDecoder extends ExpressionGenotypeDecoder {
 		zoom = egParameters[EG_ZOOM_INDEX].value;
 		polar = egParameters[EG_POLAR_COORDINATES_INDEX].value;
 		grayscale = egParameters[EG_GRAYSCALE_INDEX].value;
-		if (grayscale == 1) {
-			samples = new Array(this.width * this.height);
-			pixels = new Array(this.width * this.height);
+		channels = (1 - grayscale) * 2 + 1;
+		samples = [];
+		pixels = [];
+		
+		for (var i = 0; i < channels; i++) { 
+			samples.push(new Array(this.width * this.height));
+			pixels.push(new Array(this.width * this.height));
 		}
-		else {
-			samples = new Array(this.width * this.height * 3);
-			pixels = new Array(this.width * this.height * 3);
-		}	
+		
+		console.log(samples);
 		
 		// Collect samples
 		
 		index = 0;
 		for (var y = 0; y < this.height; y++) {
 			for(var x = 0; x < this.width; x++) {
-				for (var color = 0; color < (1-grayscale) * 2 + 1; color++) {
-					x1 = (width / this.width) * (x - this.width/2) / zoom;
-					y1 = (height / this.height) * (y - this.height/2) / zoom;
-					if (polar == 1) {
-						temp = x1;
-						x1 = Math.sqrt(Math.pow(x1,2) + Math.pow(y1,2));
-						y1 = Math.atan2(y1,temp);
-						lambda = (y1 + Math.PI)/(2*Math.PI);
-						if (grayscale == 0) {
+				x1 = (width / this.width) * (x - this.width/2) / zoom;
+				y1 = (height / this.height) * (y - this.height/2) / zoom;
+				if (polar == 1) {
+					temp = x1;
+					x1 = Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2));
+					y1 = Math.atan2(y1, temp);
+					lambda = (y1 + Math.PI)/(2 * Math.PI);
+					if (channels > 1) {
+						for (var color = 0; color < channels; color++) {
 							res1 = expressionGenotype.exp.evaluate([x1, (y1 - Math.PI)/2, color]); 
 							res2 = expressionGenotype.exp.evaluate([x1, (y1 + Math.PI)/2, color]);
+							samples[color][index] = lambda * res1 + (1 - lambda) * res2;
 						}
-						else {
-							res1 = expressionGenotype.exp.evaluate([x1, (y1 - Math.PI)/2]); 
-							res2 = expressionGenotype.exp.evaluate([x1, (y1 + Math.PI)/2]);
-						}
-						samples[index++] = lambda * res1 + (1-lambda) * res2;
 					}
 					else {
-						if (grayscale == 0) 
-							samples[index++] = expressionGenotype.exp.evaluate([x1, y1, color]);
-						else
-							samples[index++] = expressionGenotype.exp.evaluate([x1, y1]);
+						res1 = expressionGenotype.exp.evaluate([x1, (y1 - Math.PI)/2]); 
+						res2 = expressionGenotype.exp.evaluate([x1, (y1 + Math.PI)/2]);
+						samples[0][index] = lambda * res1 + (1 - lambda) * res2;
 					}
+
 				}
-//				samples[index++] = 1000*x1; // DEBUG
+				else {
+					if (channels > 1) 
+						for (var color = 0; color < channels; color++) 
+							samples[color][index] = expressionGenotype.exp.evaluate([x1, y1, color]);
+					else
+						samples[0][index] = expressionGenotype.exp.evaluate([x1, y1]);
+				}
+				index++;
 			}
 		}
-				
+
 		// Normalize samples
 		
-		pixels = this.zscoreNorm(samples);
+		for (var color = 0; color < channels; color++)
+			pixels[color] = this.zscoreNorm(samples[color]);
 		
 		// Saturate pixels 
 		
-		for (var i = 0; i < pixels.length; i++) {
-			if (pixels[i] > 255) pixels[i] = 255;
-			if (pixels[i] < 0) pixels[i] = 0;
-		}
+		for (var color = 0; color < channels; color++) 
+			for (var i = 0; i < pixels.length; i++) {
+				if (pixels[color][i] > 255) pixels[color][i] = 255;
+				if (pixels[color][i] < 0) pixels[color][i] = 0;
+			}
 		
 		// Create image
 		
 		index = 0;
 		for (var i = 0; i < this.height * this.width; i++) {
-			if (grayscale == 1) {
-				for (var color = 0; color < 3; color++) {
-					rawImage.imageData.data[index++] = pixels[i];
-				}
-				rawImage.imageData.data[index++] = 255;
-			}
-			else {
-				for (var color = 0; color < 3; color++) {
-					rawImage.imageData.data[index++] = pixels[i * 3 + color];
-				}
-				rawImage.imageData.data[index++] = 255;
-			}
+			for (var color = 0; color < 3; color++) 
+				rawImage.imageData.data[index++] = pixels[color * (1 - grayscale)][i];
+			rawImage.imageData.data[index++] = 255;
 		}	
 		return rawImage;
 	}
